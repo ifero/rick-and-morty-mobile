1ccdf543a50e1d6a75da4b6be87024dcfad12ca5
Andrea Pacino <andrea.pacino87@gmail.com>
2022-08-12 18:56:41 +0200

    feat: initial commit for RN + TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['import', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  rules: {
+    'import/no-named-as-default': 'off',
+    'import/no-duplicates': 'error',
+    'import/first': 'error',
+    'import/no-unresolved': 'error',
+    'import/no-named-as-default-member': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'never',
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+  globals: {
+    JSX: true,
+    Element: true,
+  },
+};
