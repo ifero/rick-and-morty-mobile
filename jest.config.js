105f9e775d14d2a6ea7151f7167c2ec6dc12c990
Andrea Pacino <andrea.pacino87@gmail.com>
2022-08-15 17:36:35 +0200

    chore: configure jest extention

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['./jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
 };
