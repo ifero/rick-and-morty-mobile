e8014634c8d5e8ae2da86679b1a1cf5788f26454
Andrea Pacino <andrea.pacino87@gmail.com>
2022-08-15 17:42:32 +0200

    feat: add basic navigation to CharactersList

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,11 +1,22 @@
-import { FC } from 'react';
-import { Text } from 'react-native';
+import { FC, useCallback } from 'react';
+import { Pressable, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationProps } from 'navigation/types';
 
 const Landing: FC = () => {
+  const { navigate } = useNavigation<NavigationProps>();
+
+  const goToList = useCallback(() => {
+    navigate('CharactersList');
+  }, [navigate]);
+
   return (
     <SafeAreaView>
       <Text>Welcome to Rick and Morty</Text>
+      <Pressable onPress={goToList}>
+        <Text>GO TO LIST!</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
